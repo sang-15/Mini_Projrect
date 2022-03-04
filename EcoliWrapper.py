ff99358f5361079461d59d827ed824210fa9f7e2
sang-15 <99695173+sang-15@users.noreply.github.com>
2022-03-04 13:32:37 -0600

    Update EcoliWrapper.py

--- EcoliWrapper.py
+++ EcoliWrapper.py
@@ -10,11 +10,11 @@ import csv
 #Setup
 
 #Create the result folder
-os.system('mkdir Results/')
+os.system('mkdir $HOME/Results/')
 
 #Setup Logging
 filename = 'miniproject.log'
-path = 'Results/'
+path = '$HOME/Results/'
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
@@ -26,21 +26,21 @@ log.addHandler(handler)
 
 
 #Retrieve the Illumina reads for the resequencing of K-12 project from NCBI:
-os.system('prefetch -v SRR8185310 --output-directory Results/')
+os.system('prefetch -v SRR8185310 --output-directory $HOME/Results/')
 
 #Convert the sra file into fastq format
-os.system('fastq-dump Results/SRR8185310/SRR8185310.sra --outdir Results/SRR8185310/')
+os.system('fastq-dump $HOME/Results/SRR8185310/SRR8185310.sra --outdir $HOME/Results/SRR8185310/')
 
 
 
 #Assembly
 
 #Using SPAdes to assemble the genome
-os.system('spades.py -k 55,77,99,127 -s Results/SRR8185310/SRR8185310.fastq -t 2 --only-assembler -o Results/spade_result/')
+os.system('spades.py -k 55,77,99,127 -s $HOME/Results/SRR8185310/SRR8185310.fastq -t 2 --only-assembler -o $HOME/Results/spade_result/')
 
 #Logging command used
 log.info('SPAdes was run with the following command to assemble the K12 genome - SRR8185310:')
-log.info('spades.py -k 55,77,99,127 -s Results/SRR8185310/SRR8185310.fastq -t 2 --only-assembler -o Results/spade_result/')
+log.info('spades.py -k 55,77,99,127 -s $HOME/Results/SRR8185310/SRR8185310.fastq -t 2 --only-assembler -o $HOME/Results/spade_result/')
 log.info(' ')
 
 #Calculate the number of contigs with a length > 1000, calculate the length of the assembly, 
@@ -48,13 +48,13 @@ log.info(' ')
 long = []
 count = 0
 length = 0
-for contig in SeqIO.parse('Results/spade_result/contigs.fasta', 'fasta'):
+for contig in SeqIO.parse('$HOME/Results/spade_result/contigs.fasta', 'fasta'):
     if len(contig.seq) > 1000:
         count += 1
         length += len(contig.seq)
         long.append(contig)
             
-SeqIO.write(long, 'Results/long.fasta', "fasta")
+SeqIO.write(long, '$HOME/Results/long.fasta', "fasta")
 
 ##Logging the number of long contigs found
 log.info('There are ' + str(count) + ' contigs > 1000 in the assembly.')
@@ -69,10 +69,10 @@ log.info(' ')
 #Annotation
 
 #Use Prokka to annotate this assembly with Prokka default Escherichia genus database.
-os.system('prokka --genus Escherichia --outdir Results/prokka_results/ Results/long.fasta')
+os.system('prokka --genus Escherichia --outdir $HOME/Results/prokka_results/ $HOME/Results/long.fasta')
 #Logging command used
 log.info('Prokka was run with following command for annotation:')
-log.info('prokka --genus Escherichia --outdir Results/prokka_results/ Results/long.fasta')
+log.info('prokka --genus Escherichia --outdir $HOME/Results/prokka_results/ $HOME/Results/long.fasta')
 log.info(' ')
 
 #Logging prokka's results
@@ -80,7 +80,7 @@ log.info('The Prokka analysis summary is list as following: ')
 log.info(' ')
 
 #Locate the txt file under Prokka
-prokka_txt = glob.glob("Results/prokka_results/*.txt")
+prokka_txt = glob.glob("$HOME/Results/prokka_results/*.txt")
 prokka_txt = ''.join(prokka_txt)
 
 #Open input file text, read input file as a string, remove the empty line at the end and split the input file by line
@@ -116,39 +116,39 @@ log.info(' ')
 #Mapping
 
 #Download data from RefSeq E. coli K-12 (NC_000913) for building indexes
-os.system('wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.fna -P Results/NC_000913/')
+os.system('wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.fna -P $HOME/Results/NC_000913/')
 
 #Use bowtie2 to build index with prefix - 'EcoliK12'
-os.system('bowtie2-build --threads 2 -q Results/NC_000913/NC_000913.fna EcoliK12')
+os.system('bowtie2-build --threads 2 -q $HOME/Results/NC_000913/NC_000913.fna EcoliK12')
 
 #Make a new folder under Result to keep all indexes generated
-os.system('mkdir Results/EcoliK12_index')
-os.system('mv EcoliK12*.bt2 Results/EcoliK12_index/')
+os.system('mkdir $HOME/Results/EcoliK12_index')
+os.system('mv EcoliK12*.bt2 $HOME/Results/EcoliK12_index/')
 
 #Download data from the E. coli transcriptome project of a K-12 derivative BW38028 
 #SRR1411276: https://www.ncbi.nlm.nih.gov/sra/SRX604287
-os.system('prefetch -v SRR1411276  --output-directory Results/')
+os.system('prefetch -v SRR1411276  --output-directory $HOME/Results/')
 
 ##Convert the sra file into fastq format
-os.system('fastq-dump Results/SRR1411276/SRR1411276.sra --outdir Results/SRR1411276/')
+os.system('fastq-dump $HOME/Results/SRR1411276/SRR1411276.sra --outdir $HOME/Results/SRR1411276/')
 
 #Use Tophat to map reads from SRR1411276 to index, and save the results to 'Results/tophat_out/'
-os.system('tophat2 -o Results/tophat_out/ --no-novel-juncs Results/EcoliK12_index/EcoliK12 Results/SRR1411276/SRR1411276.fastq')
+os.system('tophat2 -o $HOME/Results/tophat_out/ --no-novel-juncs $HOME/Results/EcoliK12_index/EcoliK12 $HOME/Results/SRR1411276/SRR1411276.fastq')
 
 
 
 #Quantification
 
 #Use Cufflinks to quantify transcriptomic expression and save the output to 'Results/Cufflinks_out/'
-os.system('cufflinks -q -p 2 -o Results/Cufflinks_out/ Results/tophat_out/accepted_hits.bam')
+os.system('cufflinks -q -p 2 -o $HOME/Results/Cufflinks_out/ $HOME/Results/tophat_out/accepted_hits.bam')
 
 #Rewrite the quantified transciptomic expression generated from Crufflinks to 'transcriptome_data.fpkm', which is a
 #csv format file with seqname, start, end, strand and FPKM for each record.
 
-fpkm = open('Results/transcriptome_data.fpkm', 'w')
+fpkm = open('$HOME/Results/transcriptome_data.fpkm', 'w')
 writer = csv.writer(fpkm, delimiter = ',')
 
-with open('Results/Cufflinks_out/transcripts.gtf') as handle:
+with open('$HOME/Results/Cufflinks_out/transcripts.gtf') as handle:
     reader = csv.reader(handle, delimiter = '\t')
     for row in reader:
 
